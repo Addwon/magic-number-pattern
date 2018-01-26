1094fe7797bbbab73be144a42b16f179f3461778
Addis <addwon@gmail.com>
2018-01-26 15:52:28 -0500

    Commit 4

--- src/main/java/week1/magicnumberpattern/MainController.java
+++ src/main/java/week1/magicnumberpattern/MainController.java
@@ -15,18 +15,19 @@ public class MainController {
         String numSequence="Starting Number: "+startingNumber+"<br/>";
 
         do{
-            if(num>0) {
+
                 if (num % 2 == 0)
                     num /= 2;
                 else if(num % 2 != 0)
                     num = (num * 3) + 1;
+                else{
+                    numSequence="Invalid input";
+                }
                 numResult = num;
                 numSequence += numResult + "  ";
-            }
-            else {
-                num = 1;
-                numSequence = "Please enter a positive integer";
-            }
+
+
+
         }while(num!=1);
 
         return numSequence;
